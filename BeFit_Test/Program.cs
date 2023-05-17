1d1b2caab76e479992b2bdba09d0898578914dd0
ilya_ <ilya_@DESKTOP-HS5MARB>
2023-05-18 03:24:37 +0800

    Добавьте файлы проекта.

---
+++ BeFit_Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFit_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartTaskAsync();
+            Console.ReadLine();
+        }
+        private static async Task StartTaskAsync()
+        {
+            var taskManager = new TaskManager();
+
+            taskManager.AddTask(() => { Console.WriteLine("Задача 1"); });
+            taskManager.AddTask(() => { Console.WriteLine("Задача 2"); });
+            taskManager.AddTask(() => { Console.WriteLine("Задача 3"); });
+
+            await taskManager.ProcessTasksAsync();
+
+            await taskManager.WaitAllTasksAsync();
+        }
+    }
+}
---
+++ BeFit_Test/TaskManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeFit_Test
+{
+    public delegate void TaskDelegate();
+
+    internal class TaskManager
+    {
+        private List<TaskDelegate> tasks;
+
+        public TaskManager()
+        {
+            tasks = new List<TaskDelegate>();
+        }
+
+        public void AddTask(TaskDelegate task)
+        {
+            tasks.Add(task);
+        }
+
+        public async Task ProcessTasksAsync()
+        {
+            foreach (var task in tasks)
+            {
+                await Task.Run(() => task());
+            }
+        }
+
+        public async Task WaitAllTasksAsync()
+        {
+            await Task.WhenAll(tasks.Select(task => Task.Run(() => task())));
+        }
+    }
+}
